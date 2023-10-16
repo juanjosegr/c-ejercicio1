7c5c72aa87f59d47f2b7610a8e6e9a345e2d68ba
juanjosegr <juanjosegr9@gmail.com>
2023-10-16 16:01:12 +0200

    a
    terminado

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,66 @@
+﻿class EditorGrafico
+{
+
+}
+
+interface IGrafico
+{
+    public bool mover(int x, int y);
+    public void dibujar();
+}
+
+
+class GraficoCompuesto: IGrafico
+{
+    private List<IGrafico> ListaGrafico;
+
+    public void dibujar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool mover(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Punto: IGrafico
+{
+    public int x {  get; private set; }
+    public int y { get; private set; }
+
+    void PuntoFun(int x, int y){
+        
+    }
+
+    public bool mover(int x, int y){
+        throw new NotImplementedException();
+    }
+
+    public void dibujar()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Circulo: Punto
+{
+    public int radio { get; private set; }
+
+    void creaCirculo (int x, int y, int radio){
+
+    }
+
+}
+
+class Rectangulo: Punto
+{
+    public int ancho { get; private set; }
+    public int alto { get; private set; }
+
+    public void creaRectangulo(int x, int y, int ancho, int alto){
+        
+    }
+
+}
